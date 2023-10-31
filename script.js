7286fd853fe2c97cd5ad6eeb616d6460b59e7a4c
aznyan <aznyan5510@gmail.com>
2023-10-31 19:11:22 -0400

    Added a turns display to see which marker goes Changed from alerting who won to displaying who won

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ function gameBoard() {
     document.addEventListener('DOMContentLoaded', function() {
         const board = document.querySelector('.board');
         const cells = [];
+
         const player1 = createUser('Player 1', 'X');
         const player2 = createUser('Player 2', 'O');
         let currentPlayer = player1;
-    
+
+        const turns = document.querySelector('#turns'); // To output players turn
+        turns.innerHTML = `${currentPlayer.marker} Starts Game!`;
+
         // Create cells
         for (let i = 0; i < 9; i++) {
             const cell = document.createElement('div');
@@ -16,16 +20,21 @@ function gameBoard() {
         }
 
         function makeMove(index) {
+            if (checkWinner(player1.marker) || checkWinner(player2.marker)) {
+                return;
+            }
+
             if (cells[index].textContent === '') {
                 cells[index].textContent = currentPlayer.marker;
                 cells[index].classList.add(currentPlayer.marker);
 
                 if (checkWinner(currentPlayer.marker)) {
-                    alert(`${currentPlayer.name} wins!`);
+                    turns.innerHTML = `Congrats ${currentPlayer.name}, ${currentPlayer.marker} Won!`;
                 } else if (checkDraw()) {
-                    alert("It's a draw");
+                    turns.innerHTML = "It's a draw!"
                 } else {
                     currentPlayer = (currentPlayer === player1) ? player2 : player1;
+                    turns.innerHTML = `It's ${currentPlayer.marker}'s turn`; // Update the turn message                
                 }
             }
         }
@@ -49,6 +58,7 @@ function gameBoard() {
                 cell.classList.remove('X', 'O');
             });
             currentPlayer = player1;
+            turns.innerHTML = `${currentPlayer.marker} Starts Game!`;
         }
 
         const resetBtn = document.querySelector('#restart');
